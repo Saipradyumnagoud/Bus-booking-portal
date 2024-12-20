a68cfb889f99cff52e7d13266fd9bdb39f762bd8
Sai pradyumna Goud Chiragoni <143107589+Saipradyumnagoud@users.noreply.github.com>
2024-12-20 13:16:49 +0530

    Added basic files

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About BookNow</h1>
+      <p>BookNow is your one-stop solution for booking tickets online.</p>
+    </div>
+  );
+};
+
+export default About;
